5a15872aba5243c691e2c8b8111af8281fc8ef52
Allen B <pingguo.ny+git@gmail.com>
2018-05-08 14:04:51 +0000

    something

--- app/methods.js
+++ app/methods.js
@@ -1,17 +1,24 @@
-function init() {
-  var config = {
-    apiKey: "AIzaSyCrjDPPAz0REniPtrs4FzlzPcnwGAKA8yQ",
-    authDomain: "something-8dccd.firebaseapp.com",
-    databaseURL: "https://something-8dccd.firebaseio.com",
-    projectId: "something-8dccd",
-    storageBucket: "something-8dccd.appspot.com",
-    messagingSenderId: "684904810173"
-  };
-  init.app = firebase.initializeApp(config);
-}
+var app = (function() {
+  function init() {
+    var config = {
+      apiKey: "AIzaSyCrjDPPAz0REniPtrs4FzlzPcnwGAKA8yQ",
+      authDomain: "something-8dccd.firebaseapp.com",
+      databaseURL: "https://something-8dccd.firebaseio.com",
+      projectId: "something-8dccd",
+      storageBucket: "something-8dccd.appspot.com",
+      messagingSenderId: "684904810173"
+    };
+    init.app = firebase.initializeApp(config);
+  }
 
-function add(name) {
-    var database = init.app.database();
-    var ref = database.ref("users/").push();
-    ref.set(name);   
-}
+  function add(name) {
+      var database = init.app.database();
+      var ref = database.ref("users/").push();
+      ref.set(name);
+  }
+  
+  return {
+    init: init,
+    add: add
+  }
+})();
