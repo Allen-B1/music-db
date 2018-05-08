f92cda7c4f75f3612f34b6b90a18780944d7f93f
Allen B <pingguo.ny+git@gmail.com>
2018-05-08 20:17:05 +0000

    stuff

--- app/methods.js
+++ app/methods.js
@@ -1,17 +1,9 @@
-var app = (function() {
-  function init() {
-    var config = {
-      apiKey: "AIzaSyCrjDPPAz0REniPtrs4FzlzPcnwGAKA8yQ",
-      authDomain: "something-8dccd.firebaseapp.com",
-      databaseURL: "https://something-8dccd.firebaseio.com",
-      projectId: "something-8dccd",
-      storageBucket: "something-8dccd.appspot.com",
-      messagingSenderId: "684904810173"
-    };
+var songdb = (function() {
+  function init(config) {
     init.app = firebase.initializeApp(config);
   }
 
-  function addSong(name) {
+  function addSong(name /*string*/) {
       var database = init.app.database();
       var songlist = database.ref("songs/");
       var ref = songlist.push();
