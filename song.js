6f3587fa29f9d80a67428aae8c88c7c580ac6009
Allen B <28495335+Allen-B1@users.noreply.github.com>
2018-05-16 14:28:01 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,20 +1,23 @@
 musicdb.init();
 
 var id = decodeURI(location.search.slice(1));
-if(id)
+if(id) {
   musicdb.get(id).then(function(data) {
     if(data != null) {
       document.title = data.name;
       var nameView = document.getElementById("name");
       nameView.innerHTML = "";
       nameView.appendChild(document.createTextNode(data.name));
-
-      if(data.audio) {
-        var audioView = document.getElementById("audio");
-        audioView.src = data.audio;
-        audioView.style.display = 'block';
-      }
     } else {
       
     }
   });
+  
+  musicdb.getAudio(id).then(function(data) {
+    if(data) {
+      var audioView = document.getElementById("audio");
+      audioView.src = data;
+      audioView.style.display = 'block';
+    }
+  });
+}
