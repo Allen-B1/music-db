b73c2eefed540daba88ad744a2f40b92dfb554e6
ALlen B <pingguo.ny@gmail.com>
2018-05-15 20:35:00 -0400

    styling

--- song.js
+++ song.js
@@ -1,20 +1,20 @@
 musicdb.init();
 
 var id = decodeURI(location.search.slice(1));
+if(id)
+  musicdb.get(id).then(function(data) {
+    if(data != null) {
+      document.title = data.name;
+      var nameView = document.getElementById("name");
+      nameView.innerHTML = "";
+      nameView.appendChild(document.createTextNode(data.name));
 
-musicdb.get(id).then(function(data) {
-  if(data != null) {
-    document.title = data.name;
-    var nameView = document.getElementById("name");
-    nameView.innerHTML = "";
-    nameView.appendChild(document.createTextNode(data.name));
-
-    if(data.audio) {
-      var audioView = document.getElementById("audio");
-      audioView.src = data.audio;
-      audioView.style.display = 'block';
+      if(data.audio) {
+        var audioView = document.getElementById("audio");
+        audioView.src = data.audio;
+        audioView.style.display = 'block';
+      }
+    } else {
+      
     }
-  } else {
-    
-  }
-});
+  });
